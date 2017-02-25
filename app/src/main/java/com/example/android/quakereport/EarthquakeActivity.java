64d2291705ffa0abf1ec9d00c75e99b9401243a1
anujRaka <anuj.factiousboy@gmail.com>
2017-02-25 20:31:30 +0530

    End of lesson 3
    Added code to check network connectivity

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -16,8 +16,11 @@
 package com.example.android.quakereport;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -79,7 +82,25 @@ public class EarthquakeActivity extends AppCompatActivity
         mEmptyStateTextView = (TextView) findViewById(R.id.empty_state);
         earthquakeListView.setEmptyView(mEmptyStateTextView);
 
-        getLoaderManager().initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        // Get a reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            getLoaderManager().initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide loading indicator so error message will be visible
+            ProgressBar loadingIndProgBar = (ProgressBar) findViewById(R.id.loading_indicator);
+            loadingIndProgBar.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
 
     }
 
